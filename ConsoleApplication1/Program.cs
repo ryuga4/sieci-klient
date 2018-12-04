93a541da0cd5fb2c5b35e131d311e2a2aa56f989
ryuga4 <ryuga4@o2.pl>
2018-12-04 16:06:53 +0100

    Sending file

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -18,6 +18,8 @@ namespace ConsoleApplication1
 
             
             var c = new FileSender();
+            var x = File.ReadAllBytes("C:\\Users\\micjan11\\dupa.txt.txt");
+            c.SendFile(x, "txt", "dwa");
             c.FilesEvent += PrintFiles;
 
 
